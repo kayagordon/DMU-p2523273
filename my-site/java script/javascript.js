543f906c13dba1a2f8ef2b316972534fe5d22096
kayagordon <133826866+kayagordon@users.noreply.github.com>
2023-05-17 01:55:45 +0100

    Add files via upload
    Added website

---
+++ my-site/java script/javascript.js
@@ -0,0 +1,14 @@
+<script>
+  var audio = document.getElementById('audioPlayer');
+  audio.play();
+</script>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+      });
+    });
+  });
+</script>
---
+++ my-site/javascript.js
@@ -0,0 +1,4 @@
+<script>
+  var audio = document.getElementById('audioPlayer');
+  audio.play();
+</script>
